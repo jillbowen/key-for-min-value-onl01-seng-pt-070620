a67f07ffda12075d024422a6cbafca8f9426347a
Jill Short <jillian.b.short@gmail.com>
2020-07-17 02:22:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  lowest_name = []
   name_hash.collect do |name, number|
     if number < 2 
-      lowest_name << name
+      name
+    else number > 1 
     end
  end
-  lowest_name
 end
